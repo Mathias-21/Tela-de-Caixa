5e1f716f55e428634f0b9e0d7e1566beb2e720f2
Mathias Lemes <tigre12374@gmail.com>
2022-02-22 18:00:56 -0400

    Criação da area de Descontos e Acréscimos

--- mains.js
+++ mains.js
@@ -8,6 +8,7 @@ const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
 // Guardando elementos do HTML em constantes
 const total = document.querySelector("#total");
 const subtotal = document.querySelector("#subtotal");
+const acrescimo = document.querySelector("#acrescimo");
 const descontos = document.querySelector("#descontos");
 const corpoTabela = document.querySelector("#corpo-tabela");
 const inputClientes = document.querySelector("#input-search-clientes");
@@ -18,7 +19,10 @@ const DropdownClientes = document.querySelector("#dropdown-clientes");
 const precoProdutoInput = document.querySelector("#preco-produto-input");
 const descontoProdutoInput = document.querySelector("#desconto-produto-input");
 // Declaração da variável do objeto de dados
-let data = [];
+const produtos = JSON.parse(localStorage.getItem("produtos"));
+
+let data = produtos || [];
+let valorVenda;
 // ================================================================================== Teclas de Atalho ==================================================================================
 // =================== Alt + 5 ===================
 const cancelarCompra = () => {
@@ -27,9 +31,7 @@ const cancelarCompra = () => {
 };
 document.addEventListener("keydown", (event) => {
   const keyCode = event.keyCode;
-  if (keyCode === 53) {
-    if (event.altKey) cancelarCompra();
-  }
+  if (keyCode === 53 && event.altKey) cancelarCompra();
 });
 document
   .querySelector("#botao-cancelar-compra")
@@ -45,22 +47,18 @@ const escolherCliente = () => {
 };
 document.addEventListener("keydown", (event) => {
   const keyCode = event.keyCode;
-  if (keyCode === 52) {
-    if (event.altKey) escolherCliente();
-  }
+  if (keyCode === 52 && event.altKey) escolherCliente();
 });
 document
   .querySelector("#btn-escolher-cliente")
   .addEventListener("click", () => escolherCliente());
 // ================================================================================== Somar os valores dos produtos ==================================================================================
 function getSubtotal(total, item) {
-  // Transformar string em number. Ex: "R$ 1,00 => 1.00"
   const subtotalNumber =
     toNumber(item.PRECO_PRODUTO) * toNumber(item.QTDE_PRODUTO);
   return total + subtotalNumber;
 }
 function getDescontos(total, item) {
-  // Transformar string em number. Ex: "R$ 1,00 => 1.00"
   const descontoNumber =
     toNumber(item.DESCONTO_PRODUTO) * toNumber(item.QTDE_PRODUTO);
   return total + descontoNumber;
@@ -82,6 +80,9 @@ const montarTabela = () => {
       </td>
       <td class='qtde-produto'> 
         ${value.QTDE_PRODUTO}
+      </td>
+      <td class='acrescimo-protuto'> 
+        ${value.ACRESCIMO_PRODUTO}
       </td>
       <td class='desconto-protuto'> 
         ${value.DESCONTO_PRODUTO}
@@ -108,51 +109,46 @@ function toNumber(value) {
 }
 // ================================================================================== Adicionar Produto ==================================================================================
 const adicionarProduto = () => {
-  if (codProdutoInput.value !== "") {
-    // Guardando os valores dos inputs em constantes
-    const valueCod = codProdutoInput.value;
-    const valueNome = nomeProdutoInput.value;
-    const valueQtde = qtdeProdutoInput.value;
-    const valuePreco = precoProdutoInput.value;
-    const valueDesconto = descontoProdutoInput.value;
-    // Formatando os valores em dinheiro(real)
-    const PrecoFormatado = formatar(valuePreco);
-    const DescontoFormatado = formatar(valueDesconto);
-    const SubtotalFormatado = formatar(
-      valuePreco * valueQtde - valueDesconto * valueQtde
-    );
-    // Criação de um novo objeto com os novos valores dos inputs adicionados
-    const newData = {
-      ID_PRODUTO: data.length,
-      COD_PRODUTO: valueCod,
-      NOME_PRODUTO: valueNome,
-      QTDE_PRODUTO: valueQtde,
-      PRECO_PRODUTO: PrecoFormatado,
-      DESCONTO_PRODUTO: DescontoFormatado,
-      SUBTOTAL_PRODUTO: SubtotalFormatado,
-    };
-    // Adicionando o novo objeto com os valores dos inputs ao "Banco de dados"
-    data.push(newData);
-    // console.log(data);
-    // Adicionando os valores somados na tela
-    const subtotalTotal = data.reduce(getSubtotal, 0);
-    const descontosTotal = data.reduce(getDescontos, 0);
-    subtotal.innerHTML = formatar(subtotalTotal);
-    descontos.innerHTML = formatar(descontosTotal);
-    total.innerHTML = formatar(subtotalTotal - descontosTotal);
-    // Adicionando a linha(tr) na tabela
-    corpoTabela.innerHTML = montarTabela();
-    // Limpando os valores dos inputs e focando no primeiro input
-    codProdutoInput.value = "";
-    nomeProdutoInput.value = "";
-    qtdeProdutoInput.value = "";
-    precoProdutoInput.value = "";
-    descontoProdutoInput.value = "";
-    codProdutoInput.focus();
-  } else {
-    alert("Por favor, coloque os dados abaixo");
-    codProdutoInput.focus();
-  }
+  // Guardando os valores dos inputs em constantes
+  const valueCod = toNumber(codProdutoInput.value);
+  const valueNome = toNumber(nomeProdutoInput.value);
+  const valuePreco = toNumber(precoProdutoInput.value);
+  const valueQtde = toNumber(qtdeProdutoInput.value);
+  const valueDesconto = 0;
+  const valueAcrescimo = 0;
+  // Formatando os valores em dinheiro(real)
+  const PrecoFormatado = formatar(valuePreco);
+  const AcrescimoFormatado = formatar(valueAcrescimo);
+  const DescontoFormatado = formatar(valueDesconto);
+  const SubtotalFormatado = formatar(valuePreco * valueQtde);
+  // Criação de um novo objeto com os novos valores dos inputs adicionados
+  const newData = {
+    ID_PRODUTO: data.length,
+    COD_PRODUTO: valueCod,
+    NOME_PRODUTO: valueNome,
+    QTDE_PRODUTO: valueQtde,
+    PRECO_PRODUTO: PrecoFormatado,
+    ACRESCIMO_PRODUTO: AcrescimoFormatado,
+    DESCONTO_PRODUTO: DescontoFormatado,
+    SUBTOTAL_PRODUTO: SubtotalFormatado,
+  };
+  // Adicionando o novo objeto com os valores dos inputs ao "Banco de dados"
+  data.push(newData);
+  localStorage.setItem("produtos", JSON.stringify(data));
+  // Adicionando os valores somados na tela
+  const subtotalTotal = data.reduce(getSubtotal, 0);
+  subtotal.innerHTML = formatar(subtotalTotal);
+  acrescimo.innerHTML = formatar(valueAcrescimo);
+  descontos.innerHTML = formatar(valueDesconto);
+  total.innerHTML = formatar(subtotalTotal);
+  // Adicionando a linha(tr) na tabela
+  corpoTabela.innerHTML = montarTabela;
+  // Limpando os valores dos inputs e focando no primeiro input
+  codProdutoInput.value = "";
+  nomeProdutoInput.value = "";
+  qtdeProdutoInput.value = "";
+  precoProdutoInput.value = "";
+  codProdutoInput.focus();
 };
 // Pegando o botão de "Adicionar produto" e passando a função de adicionarProduto
 document
@@ -168,10 +164,10 @@ function apagarProduto(id) {
   subtotal.innerHTML = formatar(subtotalTotal);
   descontos.innerHTML = formatar(descontosTotal);
   total.innerHTML = formatar(subtotalTotal - descontosTotal);
+  localStorage.setItem("produtos", JSON.stringify(data));
 }
 // ================================================================================== Selecionar Linha ==================================================================================
 const selecionarLinha = (id) => {
-  // const numLinha = id.replace("linha", "");
   const linhaEspec = document.querySelector(`#${id}`);
   linhaEspec.classList.toggle("selecionado");
 };
@@ -195,11 +191,10 @@ const retirarItem = () => {
 const btnRetirarItem = document
   .querySelector("#btn-retirar-item")
   .addEventListener("click", retirarItem);
+
 document.addEventListener("keydown", (event) => {
   const keyCode = event.keyCode;
-  if (keyCode === 56) {
-    if (event.altKey) retirarItem();
-  }
+  if (keyCode === 56 && event.altKey) retirarItem();
 });
 // ================================================================================== Filtrar Vendedores ==================================================================================
 const inputVendedores = document.querySelector("#input-search-vendedores");
@@ -221,10 +216,10 @@ function filtrarVendedores() {
 // Adicionar o nome do p clicado ao nome do dropdown de vendedores
 const vendedores = document.querySelectorAll("#dropdown-vendedores-area > p");
 for (let i = 0; i < vendedores.length; i++) {
-  vendedores[i].addEventListener(
-    "click",
-    () => (DropdownVendedores.innerHTML = vendedores[i].textContent)
-  );
+  vendedores[i].addEventListener("click", () => {
+    DropdownVendedores.innerHTML = vendedores[i].textContent;
+    inputVendedores.value = "";
+  });
 }
 // ================================================================================== Filtrar Clientes ==================================================================================
 // Focar no input de pesquisar clientes ao clicar no dropdown de clientes
@@ -246,7 +241,102 @@ const clientes = document.querySelectorAll("#dropdown-clientes-area > p");
 for (let i = 0; i < clientes.length; i++) {
   clientes[i].addEventListener("click", () => {
     DropdownClientes.innerHTML = clientes[i].textContent;
-    // inputClientes.value = clientes[i].textContent;
     inputClientes.value = "";
   });
 }
+
+// ================================================================================== Modal de Descontos ==================================================================================
+const inputDescontoDinheiroTotal = document.querySelector(
+  "#input-desconto-dinheiro-total"
+);
+const inputDescontoPorcentagemTotal = document.querySelector(
+  "#input-desconto-porcentagem-total"
+);
+const inputAcrescimoDinheiroTotal = document.querySelector(
+  "#input-acrescimo-dinheiro-total"
+);
+const inputAcrescimoPorcentagemTotal = document.querySelector(
+  "#input-acrescimo-porcentagem-total"
+);
+const inputValorTotal = document.querySelector("#input-valor-total");
+const inputValorAlteradoTotal = document.querySelector(
+  "#input-valor-alterado-total"
+);
+const salvarDescontosAcrescimos = document.querySelector(
+  "#salvar-descontos-acrescimos"
+);
+
+let descontoTotal = 0;
+let descontoPorcentagemTotal = 0;
+let acrescimoTotal = 0;
+let acrescimoPorcentagemTotal = 0;
+// ====================================== Descontos ======================================
+inputDescontoDinheiroTotal.addEventListener("blur", () => {
+  valorVenda = data.reduce(getSubtotal, 0);
+  descontoTotal = Number(inputDescontoDinheiroTotal.value);
+  valorVenda -= descontoTotal;
+  inputValorAlteradoTotal.innerHTML = formatar(valorVenda);
+});
+inputDescontoPorcentagemTotal.addEventListener("blur", () => {
+  if (descontoTotal === 0 || undefined) {
+    valorVenda = data.reduce(getSubtotal, 0);
+    descontoPorcentagemTotal = Number(inputDescontoPorcentagemTotal.value);
+    const valorDinheiro = (valorVenda / 100) * descontoPorcentagemTotal;
+    valorVenda -= valorDinheiro;
+    descontoPorcentagemTotal = valorDinheiro;
+    inputValorAlteradoTotal.innerHTML = formatar(valorVenda);
+  }
+});
+// ====================================== Acréscimos ======================================
+inputAcrescimoDinheiroTotal.addEventListener("blur", () => {
+  if (descontoTotal === 0 && descontoPorcentagemTotal === 0) {
+    valorVenda = data.reduce(getSubtotal, 0);
+  }
+  acrescimoTotal = Number(inputAcrescimoDinheiroTotal.value);
+  if (acrescimoTotal !== 0) {
+    valorVenda += acrescimoTotal;
+  }
+  inputValorAlteradoTotal.innerHTML = formatar(valorVenda);
+});
+inputAcrescimoPorcentagemTotal.addEventListener("blur", () => {
+  if (
+    descontoTotal === 0 &&
+    descontoPorcentagemTotal === 0 &&
+    acrescimoTotal === 0
+  ) {
+    valorVenda = data.reduce(getSubtotal, 0);
+  }
+  if (acrescimoTotal === 0 || undefined) {
+    acrescimoPorcentagemTotal = Number(inputAcrescimoPorcentagemTotal.value);
+    const valorDinheiro = (valorVenda / 100) * acrescimoPorcentagemTotal;
+    valorVenda += valorDinheiro;
+    acrescimoPorcentagemTotal = valorDinheiro;
+    inputValorAlteradoTotal.innerHTML = formatar(valorVenda);
+  }
+});
+// ====================================== Salvar Alterações ======================================
+salvarDescontosAcrescimos.addEventListener("click", () => {
+  if (descontoTotal === 0) {
+    descontos.innerHTML = formatar(descontoPorcentagemTotal);
+  } else {
+    descontos.innerHTML = formatar(descontoTotal);
+  }
+  if (acrescimoTotal === 0) {
+    acrescimo.innerHTML = formatar(acrescimoPorcentagemTotal);
+  } else {
+    acrescimo.innerHTML = formatar(acrescimoTotal);
+  }
+  if (valorVenda !== undefined) {
+    total.innerHTML = formatar(valorVenda);
+  }
+});
+
+const subtotalTotal = data.reduce(getSubtotal, 0);
+const descontosTotal = 0;
+corpoTabela.innerHTML = montarTabela();
+subtotal.innerHTML = formatar(subtotalTotal);
+acrescimo.innerHTML = formatar(acrescimoTotal);
+descontos.innerHTML = formatar(descontosTotal);
+total.innerHTML = formatar(subtotalTotal);
+inputValorTotal.innerHTML = formatar(subtotalTotal);
+inputValorAlteradoTotal.innerHTML = formatar(subtotalTotal);
